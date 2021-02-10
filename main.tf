6a2a067b7b1239250b48d448bdac7a87fb0c25ff
Marcin Wozniak <wozik@pm.me>
2021-02-10 23:34:59 +0100

    ingress for ssh update

--- main.tf
+++ main.tf
@@ -31,7 +31,7 @@ resource "aws_security_group" "hashicat" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["10.0.10.0/24"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
