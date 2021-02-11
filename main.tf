97371c2d59444aae5280c0c926301cfc2caa50d2
Marcin Wozniak <wozik@pm.me>
2021-02-11 17:41:05 +0100

    updated cidr bloks for ingress ssh

--- main.tf
+++ main.tf
@@ -31,7 +31,7 @@ resource "aws_security_group" "hashicat" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["54.157.0.0/24"]
+    cidr_blocks = ["75.2.98.97/32"]
   }
 
   ingress {
